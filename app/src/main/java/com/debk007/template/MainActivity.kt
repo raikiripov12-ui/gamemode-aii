cd9255d40c4b12b7a2436d86a4a47a32a165e2f3
raikiripov12-ui <raikiripov12@gmail.com>
2026-04-16 13:56:05 -0300

    Update MainActivity.kt

--- app/src/main/java/com/debk007/template/MainActivity.kt
+++ app/src/main/java/com/debk007/template/MainActivity.kt
@@ -3,18 +3,14 @@ package com.debk007.template
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MaterialTheme {
-                Surface {
-                    Text("GameMode AI - OK")
-                }
-            }
+            Text("GameMode AI OK - Running")
         }
     }
 }
